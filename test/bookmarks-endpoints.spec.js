6c6bdaac66c0afc80b494ec6d0244c9c61a5ac15
Charita Nichols <nichols.charita@gmail.com>
2020-06-11 16:33:50 -0400

    changes

--- test/bookmarks-endpoints.spec.js
+++ test/bookmarks-endpoints.spec.js
@@ -8,11 +8,46 @@ describe('Bookmarks Endpoints', function () {
     before('make knex instance', () => {
         db = knex({
             client: 'pg',
-            connection: process.env.TEST_DB_URL,
+            connection: process.env.DB_URL,
         })
+        app.set('db', db)
     })
 
     after('disconnect from db', () => db.destroy())
 
     before('clean the table', () => db('bookmarks').truncate())
-})
+
+    context('Given there are bookmarks in the database', () => {
+        const testBookmarks = [
+            {
+                id: 1,
+                title: 'first test bookmark',
+                url: 'https://www.test.com',
+                rating: 3,
+                description: 'test desc',
+            },
+            {
+                id: 2,
+                title: 'second test bookmark',
+                url: 'https://www.test.com',
+                rating: 4,
+                description: 'test desc',
+            },
+            {
+                id: 3,
+                title: 'third test bookmark',
+                url: 'https://www.test.com',
+                rating: 2,
+                description: 'test desc',
+            },
+        ]
+        this.beforeEach('insert bookmarks', () => {
+            return db
+                .into('bookmarks')
+                .insert(testBookmarks)
+        })
+        it('GET / responds with 200 and all of the bookmarks', () => {
+            return supertest(app).get('/bookmarks').expect(200);
+        });
+    });
+});
