2d32d8518ca351064b4b695114298f0b6b9d11fe
Charita Nichols <nichols.charita@gmail.com>
2020-06-11 15:45:11 -0400

    test file

--- test/bookmarks-endpoints.spec.js
+++ test/bookmarks-endpoints.spec.js
@@ -1,1 +1,18 @@
 const { expect } = require('chai');
+const knex = require('knex')
+const app = require('../src/app')
+
+describe('Bookmarks Endpoints', function () {
+    let db
+
+    before('make knex instance', () => {
+        db = knex({
+            client: 'pg',
+            connection: process.env.TEST_DB_URL,
+        })
+    })
+
+    after('disconnect from db', () => db.destroy())
+
+    before('clean the table', () => db('bookmarks').truncate())
+})
